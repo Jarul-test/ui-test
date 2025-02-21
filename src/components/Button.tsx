c9dfed9c096cb7c2255c37b40e6ca009211a3837
jarul-test <jaruldev@gmail.com>
2025-02-21 18:25:54 +0530

    feat: added new prop for button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,14 @@ import React from "react";
 type ButtonProps = {
   label: string;
   onClick: () => void;
+  classname: string;
 };
 
-const Button = ({ label, onClick }: ButtonProps) => {
+const Button = ({ label, onClick, classname }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="bg-blue-500 text-white px-4 py-2 rounded"
+      className={`bg-blue-400 text-white px-4 py-2 rounded ${classname}`}
     >
       {label}
     </button>
